7f8415ea15573ec5d054d9e9e11e9593fe45a21e
Victor Usoltsev <bernarden@mail.ru>
2016-11-03 08:51:53 +1300

    added support for VMSS

--- dingo_job_types.js
+++ dingo_job_types.js
@@ -5,6 +5,32 @@ var armCompute = require('./arm_compute_ops');
 
 exports = module.exports = {};
 
+exports.vmss = {
+    operations: {
+        start: {
+            op: armCompute.vmssOperations.start_vm
+        },
+        stop: {
+            op: armCompute.vmssOperations.stop_vm
+        },
+        restart: {
+            op: armCompute.vmssOperations.restart_vm,
+            allowRandom: true
+        },
+        powercycle: {
+            op: [armCompute.vmssOperations.stop_vm, armCompute.vmssOperations.start_vm],
+            args: {
+                duration: {
+                    type: 'int-range',
+                    defaultValue: 60,
+                    includeInCall: false
+                }
+            },
+            allowRandom: true
+        }
+    }
+};
+
 exports.vm = {
     operations: {
         start: {
@@ -18,8 +44,8 @@ exports.vm = {
             allowRandom: true
         },
         powercycle: {
-            op: [ armCompute.vmOperations.stop_vm, armCompute.vmOperations.start_vm ],
-            args: { 
+            op: [armCompute.vmOperations.stop_vm, armCompute.vmOperations.start_vm],
+            args: {
                 duration: {
                     type: 'int-range',
                     defaultValue: 60,
